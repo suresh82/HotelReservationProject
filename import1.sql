8dff7d3b730799ef51d7b28abae58a251ab59cff
suresh82 <madhusuresh936@gmail.com>
2020-10-21 19:02:25 +0530

    Create import1.sql

---
+++ import1.sql
@@ -0,0 +1,4 @@
+insert into hotel values(1,'30.0','Bangolor','2020-10-25','Novatel','500','NON-AC','Available')
+insert into hotel values(2,'50.0','Chennai','2020-10-22','HotelGrand','600','AC','Available')
+insert into hotel values(3,'60.0','Hyderabad','2020-10-31','HotelTaj','700','AC','Available')
+insert into hotel values(4,'60.0','Delhi','2020-10-27','Novatel','700','AC','Available')
